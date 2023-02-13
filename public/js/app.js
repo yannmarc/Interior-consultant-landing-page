7aa98a1a28ab0a6449d9fe7448a2026e1da206e5
yannmarc <chamackstone@gmail.com>
2023-02-13 18:07:44 +0100

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+// Working on the buttons functionality
+
+// Selecting the buttons
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const navContainer = document.querySelector('.nav-container');
+
+openBtn.addEventListener("click", () => {
+    console.log("Hello console")
+})
